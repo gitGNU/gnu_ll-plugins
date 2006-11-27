c6481e1fcfd8a587ae408f5b5efed2c891d0a932
Lars Luthman <lars.luthman@gmail.com>
2006-11-27 02:56:55 +0000

    Added forgotten files

---
+++ extensions/program/lv2-program-gtk2gui.h
@@ -0,0 +1,63 @@
+/************************************************************************
+
+   This file is partly based on dssi.h from DSSI 0.9, which is
+   Copyright (C) 2004 Chris Cannam, Steve Harris and Sean Bolton
+
+   Modifications (C) 2006 Lars Luthman <lars.luthman@gmail.com>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public License
+   as published by the Free Software Foundation; either version 2.1 of
+   the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+   USA.
+
+*************************************************************************/
+
+#ifndef LV2_PROGRAM_GTK2GUI_H
+#define LV2_PROGRAM_GTK2GUI_H
+
+#include <lv2-gtk2gui.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+typedef struct {
+  
+  void (*set_program)(LV2UI_Controller, unsigned char number);
+
+} LV2_ProgramControllerDescriptor;
+
+
+typedef struct {
+
+  void (*add_program)(LV2UI_Handle handle, 
+                      unsigned char number,
+                      const char* name);
+
+  void (*remove_program)(LV2UI_Handle handle,
+                         unsigned char number);
+  
+  void (*clear_programs)(LV2UI_Handle handle);
+  
+  void (*set_program)(LV2UI_Handle handle, unsigned char number);
+
+} LV2_ProgramUIDescriptor;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
