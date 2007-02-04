04ca19a1c31f595c59c57cdeb145c68563ad91b9
Lars Luthman <lars.luthman@gmail.com>
2007-02-04 11:53:31 +0000

    Removed experimental code, it builds again

--- plugins/euphoria/distortion.hpp
+++ plugins/euphoria/distortion.hpp
@@ -4,32 +4,6 @@
 #include <stdint.h>
 
 
-#define DENORMALIZE(fv) (fv<.00000001f && fv>-.00000001f)?0:(fv)
-#define	PI	3.14159265358979323846f
-
-
-class filt1 {
-  friend class filt_lp;
-public:
-  filt1();
-  ~filt1(){};
-  float	lp();
-  float	bp();
-  float	hp();
-  void	clock(float input);
-  void	setparam(float cutoff, float q, float samplerate);
-  void	set_samplerate(float samplerate);
-private:
-  float	fs;		// sampling freq
-  float	fc;		// cutoff freq
-  float	q;		// resonance
-  float	m_f,m_q,m_qnrm;
-  float	m_h;	// hp out
-  float	m_b;	// bp out
-  float	m_l;	// lp out
-};
-
-
 class Distortion {
 public:
   
@@ -42,12 +16,7 @@ public:
 private:
   
   uint32_t m_rate;
-  float oldmix;
-  float odmix;
-  bool odchanged;
-  filt1 fuzz_filt;
-  filt1 body_filt;
-  
+
 };
 
 
