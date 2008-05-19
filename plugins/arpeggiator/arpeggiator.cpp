0422166f11402eb55aaa3b9fe542d6d579240c34
Lars Luthman <lars.luthman@gmail.com>
2008-05-19 20:13:52 +0200

    Removed pattern length port and made the arpeggiator work in both directions

--- plugins/arpeggiator/arpeggiator.cpp
+++ plugins/arpeggiator/arpeggiator.cpp
@@ -35,7 +35,7 @@ class Arpeggiator : public LV2::Plugin<Arpeggiator, LV2::URIMap<true> > {
 public:
   
   Arpeggiator(double rate) 
-    : LV2::Plugin<Arpeggiator, LV2::URIMap<true> >(5),
+    : LV2::Plugin<Arpeggiator, LV2::URIMap<true> >(4),
       m_rate(rate),
       m_num_keys(0),
       m_frame_counter(0),
@@ -43,7 +43,8 @@ public:
       m_last_key(0),
       m_midi_type(uri_to_id(LV2_EVENT_URI, 
 			    "http://lv2plug.in/ns/ext/midi#MidiEvent")),
-      m_running(false) {
+      m_running(false),
+      m_upwards(true) {
     
   }
   
@@ -58,12 +59,11 @@ public:
   void run(uint32_t nframes) {
     
     float& npb = *p(0);
-    float& plength = *p(1);
-    float& direction = *p(2);
+    m_upwards = *p(1) > 0.5;
     
     LV2_Event_Iterator iter_in, iter_out;
-    lv2_event_begin(&iter_in, p<LV2_Event_Buffer>(3));
-    LV2_Event_Buffer* outbuf = p<LV2_Event_Buffer>(4);
+    lv2_event_begin(&iter_in, p<LV2_Event_Buffer>(2));
+    LV2_Event_Buffer* outbuf = p<LV2_Event_Buffer>(3);
     lv2_event_buffer_reset(outbuf, outbuf->stamp_type, outbuf->data);
     lv2_event_begin(&iter_out, outbuf);
     
@@ -97,8 +97,14 @@ public:
           for ( ; pos < m_num_keys && m_keys[pos] != key; ++pos);
           if (pos < m_num_keys) {
             --m_num_keys;
-            if (m_next_key > pos)
+            if (m_upwards && m_next_key > pos)
               --m_next_key;
+	    else if (!m_upwards && m_next_key == pos) {
+	      if (m_next_key == 0)
+		m_next_key = m_num_keys - 1;
+	      else
+		--m_next_key;
+	    }
             for (unsigned i = pos; i < m_num_keys; ++i)
               m_keys[i] = m_keys[i + 1];
           }
@@ -118,7 +124,13 @@ public:
 	    if (pos == m_num_keys - 1 && m_next_key == 0)
 	      m_next_key = pos;
           }
-	  m_running = true;
+	  if (!m_running) {
+	    m_running = true;
+	    if (m_upwards)
+	      m_next_key = 0;
+	    else
+	      m_next_key = m_num_keys - 1;
+	  }
         }
         
       }
@@ -156,7 +168,10 @@ public:
       data[1] = m_keys[m_next_key];
       lv2_event_write(midi_out, frame, 0, m_midi_type, 3, data);
       m_last_key = data[1];
-      m_next_key = (m_next_key + 1) % m_num_keys;
+      if (m_upwards)
+	m_next_key = (m_next_key + 1) % m_num_keys;
+      else
+	m_next_key = (m_next_key + m_num_keys - 1) % m_num_keys;
     }
     
     m_frame_counter = frame - to;
@@ -173,6 +188,7 @@ protected:
   unsigned char m_last_key;
   uint32_t m_midi_type;
   bool m_running;
+  bool m_upwards;
   
 };
 
